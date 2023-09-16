14f69f4a2b26dd444b02f173cf51eb24dfe25882
unknown <douggouthro@gmail.com>
2023-09-15 21:28:42 -0500

    finished code

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+// app.get('/', (req, res) =>
+// res.sendFile(path.join(__dirname, 'public/index.html'))
+// );
 
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, 'public/notes.html'))
@@ -22,21 +24,24 @@ app.get("/api/notes",(req, res)=>{
   var data = fs.readFileSync(path.join(__dirname, 'db', 'db.json'))
   var myObject= JSON.parse(data)
   console.log(myObject)
-    res.json(data)
+    res.json(myObject)
 })
 
 
 app.post("/api/notes", (req,res)=> {
-  var data = fs.writeFile(path.join(__dirname, 'db', 'db.json'),[],(error) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
+  console.log(req.body)
+  var oldNotes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json')))
+  oldNotes.push({
+    title:req.body.title,
+    text:req.body.text
   })
-  res.json("WHATS UP!?!?!")
+  fs.writeFileSync(path.join(__dirname, 'db', 'db.json'),JSON.stringify(oldNotes))
+  res.json({message:"Added new note."})
 })
 
 //app.delete("/api/notes", (req, res)=>{})//EC
 app.get("*",(req, res)=>{
-    res.json("Sorry!")
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 
